83515e4988931cfef0259c4025173baa92d2c7aa
Justin La <justinla@uw.edu>
2020-02-03 14:21:49 -0800

    logo & timeout on contentscript

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,2 +1,6 @@
-let problemName = document.querySelector('div[data-cy="question-title"]').innerHTML;
-chrome.runtime.sendMessage({problemName: problemName});
+// temporary timeout. find better way to dynamically look for element
+setTimeout(function(){
+    let problemName = document.querySelector('div[data-cy="question-title"]');
+    if (problemName) chrome.runtime.sendMessage({problemName: problemName.innerHTML});
+}, 1000); 
+
