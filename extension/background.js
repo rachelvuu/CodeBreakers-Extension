a7484c0a3bd852974814735616b070840fa85a1e
Justin La <justinla@uw.edu>
2020-02-09 23:04:46 -0800

    prototype hint menu

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,3 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlContains: 'leetcode.com/problems/'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) { 
     console.log("request: ", request.problemName)
