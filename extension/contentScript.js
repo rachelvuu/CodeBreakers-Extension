49a3d8fd4c64001a788ab9463c9a3677e3de8556
Justin La <justinla@uw.edu>
2020-04-01 22:14:55 -0700

    cleaner code

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,11 +1,12 @@
 // for changeWindow
 const width  = window.innerWidth;
 let expandedWindow = false;
+console.log(innerWidth);
 
 // content script loads faster than rest of content, so we must wait for the page to load first.
 setTimeout(main, 1000);
 
-async function request() {
+async function requestToSpreadSheet() {
     const response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1tnGJ2eI1SkpJJMcmYfnjsW-yeGcQaf251eJgiJLC6Qo/values/Sheet1?key=AIzaSyCjjXAOcyX1Q-RzzXwg3h5-sM_JaiBDk68");
     const json = await response.json();
     return json.values;
@@ -13,22 +14,68 @@ async function request() {
 
 function changeWindow() {
     let windowSlider = document.querySelector('div[class="side-tools-wrapper__1TS9"]');
-    // expands to 2/3 width
+    // expands to 3/5 width
     if (!expandedWindow) {
-        windowSlider.setAttribute('style', `overflow: hidden; flex: 0 1 ${Math.ceil(width * 2/3)}px`);
+        windowSlider.setAttribute('style', `overflow: hidden; flex: 0 1 ${Math.ceil(width * 3/5)}px`);
     // contracts to 1/2 width
     } else { 
-        windowSlider.setAttribute('style', `overflow: hidden; flex: 0 1 ${Math.ceil(width * 1/2)}px`);
+        windowSlider.setAttribute('style', `overflow: hidden; flex: 0 1 ${Math.ceil(width * 45/100)}px`);
     }
     expandedWindow = !expandedWindow;
 }
 
+// Replaces the value of entry problemData entry to the actual HTML that will be injected.
+// Check problemData object structure for clarity.
+async function insertCardBody(problemData, hint) {
+    let htmlOfHintType;
+
+    // Different hint types require different HTML
+    if (hint.includes('Hint')) {
+        htmlOfHintType = problemData[hint];
+    } else if (hint.includes('Text')) {
+        htmlOfHintType = problemData[hint];
+    } else if (hint.includes('Video')) {
+        htmlOfHintType = `
+            <iframe src="https://www.youtube.com/embed/${problemData[hint].split('=')[1]}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `;
+    } else if (hint.includes('Code')) {
+        let response = await fetch(problemData[hint]);
+        if (response.status == 200) {
+            let code = await response.text()
+            htmlOfHintType = `
+                <button id="expandCode" type="button" class="btn btn-info mb-2 btn-block btn-lg">Expand Code Solution</button>
+                <pre class=prettyprint>
+${code.substring(code.indexOf("class Solution:")).trim()}
+                </pre>
+            `;
+        }
+    }
+
+    let hintNameForHTML = hint.replace(/ /g, '');    
+    // Base HTML for a card body
+    let cardBodyTemplate = `
+    <div class="card">
+    <div class="card-header" id="heading${hintNameForHTML}" data-toggle="collapse" data-target="#collapse${hintNameForHTML}" aria-expanded="false" aria-controls="collapse${hintNameForHTML}">
+        <h5 class="hint">
+            <div class='btn collapsed'>${hint}</div>
+        </h5>
+    </div>
+    <div id="collapse${hintNameForHTML}" class="collapse" aria-labelledby="heading${hintNameForHTML}" data-parent="#accordion">
+        <div class="card-body">
+${htmlOfHintType}
+        </div>
+    </div>
+    </div>    
+    `;
+    return cardBodyTemplate;
+}
+
 async function main() {
-    // make request to spreadsheet
-    let spreadsheet = await request();
+    // Make request to spreadsheet
+    let spreadsheet = await requestToSpreadSheet();
     // find problem title
-    let problemName = document.querySelector('div[data-cy="question-title"]');
-    let problemNameText = problemName.textContent.split('.')[1].substring(1)
+    let problemNameElem = document.querySelector('div[data-cy="question-title"]');
+    let problemNameText = problemNameElem.textContent.split('.')[1].substring(1)
     let problemRow;
     // match spreadsheet request data to problem title
     for (let row of spreadsheet) {
@@ -44,75 +91,80 @@ async function main() {
     problemData['Text Solution'] = problemRow[3]
     problemData['Video Solution'] = problemRow[4]
     problemData['Code Solution'] = problemRow[5]
-    // remove pairs that have empty values (no data in column for that row)
-    Object.keys(problemData).forEach(key => problemData[key] == '' ? delete problemData[key] : {});
-    let htmlContent = ['','','','',''];
-    // populate appropriate amt of html content based on how many filled in cells there are 
-    for (let i = 0; i < Object.keys(problemData).length; i++) {
-        let key = Object.keys(problemData)[i]
-        let currentHint;
-        if (key.includes("Video")) {
-            currentHint = `
-                <div class="card">
-                    <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-                        <h5 class="hint">
-                            <div class='btn collapsed'>${key}</div>
-                        </h5>
-                    </div>
-                    <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
-                        <div class="card-body">
-                            <iframe src="https://www.youtube.com/embed/${problemData[key].split('=')[1]}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            `
-        } else if (key.includes("Code")) {
-            let response = await fetch(problemData[key]);
-            if (response.status == 200) {
-                let data = await response.text()  
-                    currentHint = `
-                        <div class="card">
-                            <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-                                <h5 class="hint">
-                                    <div class='btn collapsed'>${key}</div>
-                                </h5>
-                            </div>
-                            <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
-                                <div class="card-body">
-                                    <button id="expandCode" type="button" class="btn btn-info mb-2 btn-block btn-lg">Expand Code Solution</button>
-                                    <pre class=prettyprint>
-${data.substring(data.indexOf("class Solution:")).trim()}
-                                    </pre>
-                                </div>
-                            </div>
-                        </div>
-                    `
-            } else {
-                currentHint = '';
-            }
-        } else {
-            currentHint = `
-                <div class="card">
-                    <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-                        <h5 class="hint">
-                            <div class='btn collapsed'>${key}</div>
-                        </h5>
-                    </div>
-                    <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
-                        <div class="card-body">
-                        ${problemData[key]}
-                        </div>
-                    </div>
-                </div>
-            `
-        }
-        htmlContent[i] = currentHint
+    
+    for (let hint in problemData) {
+        problemData[hint] = await insertCardBody(problemData, hint);
     }
-    let titleBar = problemName.parentElement;
+
+//     // remove pairs that have empty values (no data in column for that row)
+//     Object.keys(problemData).forEach(key => problemData[key] == '' ? delete problemData[key] : {});
+//     let htmlContent = ['','','','',''];
+//     // populate appropriate amt of html content based on how many filled in cells there are 
+//     for (let i = 0; i < Object.keys(problemData).length; i++) {
+//         let key = Object.keys(problemData)[i]
+//         let currentHint;
+//         if (key.includes("Video")) {
+//             currentHint = `
+//                 <div class="card">
+//                     <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+//                         <h5 class="hint">
+//                             <div class='btn collapsed'>${key}</div>
+//                         </h5>
+//                     </div>
+//                     <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
+//                         <div class="card-body">
+//                             <iframe src="https://www.youtube.com/embed/${problemData[key].split('=')[1]}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//                         </div>
+//                     </div>
+//                 </div>
+//             `
+//         } else if (key.includes("Code")) {
+//             let response = await fetch(problemData[key]);
+//             if (response.status == 200) {
+//                 let data = await response.text()  
+//                     currentHint = `
+//                         <div class="card">
+//                             <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+//                                 <h5 class="hint">
+//                                     <div class='btn collapsed'>${key}</div>
+//                                 </h5>
+//                             </div>
+//                             <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
+//                                 <div class="card-body">
+//                                     <button id="expandCode" type="button" class="btn btn-info mb-2 btn-block btn-lg">Expand Code Solution</button>
+//                                     <pre class=prettyprint>
+// ${data.substring(data.indexOf("class Solution:")).trim()}
+//                                     </pre>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     `
+//             } else {
+//                 currentHint = '';
+//             }
+//         } else {
+//             currentHint = `
+//                 <div class="card">
+//                     <div class="card-header" id="heading${i}" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+//                         <h5 class="hint">
+//                             <div class='btn collapsed'>${key}</div>
+//                         </h5>
+//                     </div>
+//                     <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
+//                         <div class="card-body">
+//                         ${problemData[key]}
+//                         </div>
+//                     </div>
+//                 </div>
+//             `
+//         }
+//         htmlContent[i] = currentHint
+//     }
+
+
+    let titleBar = problemNameElem.parentElement;
     let div = document.createElement("div");
     div.innerHTML = `
-    <link rel="stylesheet" type="text/css" href="prettify.css">
-    <script type="text/javascript" src="prettify.js"></script>
     <div class="badge badge-info mt-2" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
         CodeBreakers Hints
     </div>
@@ -120,11 +172,11 @@ ${data.substring(data.indexOf("class Solution:")).trim()}
     <div class="collapse" id="collapseExample">
         <div class='mt-1'>
             <div id="accordion">
-                ${htmlContent[0]}
-                ${htmlContent[1]}
-                ${htmlContent[2]}
-                ${htmlContent[3]}
-                ${htmlContent[4]}
+                ${problemData['Hint 1']}
+                ${problemData['Hint 2']}
+                ${problemData['Text Solution']}
+                ${problemData['Video Solution']}
+                ${problemData['Code Solution']}
                 <div class="card">
                     <div class="card-header" id="headingSix" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                         <h5 class="hint">
